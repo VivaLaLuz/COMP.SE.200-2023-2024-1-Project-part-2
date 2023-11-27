ad34bec0ffe35b87192111eb11608eb381126e71
Lassi Säike <lassisaike11@gmail.com>
2023-11-27 15:00:04 +0200

    Add tests for passing empty arrays or null/undefined indices to slice

--- test/slice.test.js
+++ test/slice.test.js
@@ -23,3 +23,29 @@ test("should handle negative indices correctly", () => {
     expect(slice(array, -3)).toEqual([3, 4, 5]);
     expect(slice(array, -4, -2)).toEqual([2, 3]);
 });
+
+test("should return an empty array if input array is empty", () => {
+    const array = [];
+
+    expect(slice(array, 1, 4)).toEqual([]);
+    expect(slice(array, 2)).toEqual([]);
+    expect(slice(array, -3, -1)).toEqual([]);
+});
+
+test("should return an empty array if input array is null or undefined", () => {
+    expect(slice(null, 1, 4)).toEqual([]);
+    expect(slice(undefined, 2)).toEqual([]);
+    expect(slice(null, -3, -1)).toEqual([]);
+});
+
+test("should handle null or undefined start or end indices correctly", () => {
+    const array = [1, 2, 3, 4, 5];
+
+    expect(slice(array, null, 4)).toEqual([1, 2, 3, 4]);
+    expect(slice(array, 2, null)).toEqual([3, 4, 5]);
+    expect(slice(array, null, null)).toEqual([1, 2, 3, 4, 5]);
+
+    expect(slice(array, undefined, 4)).toEqual([1, 2, 3, 4]);
+    expect(slice(array, 2, undefined)).toEqual([3, 4, 5]);
+    expect(slice(array, undefined, undefined)).toEqual([1, 2, 3, 4, 5]);
+});
